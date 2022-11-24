c736d0d9f957ea73d0aa4889706da1ac1136f17d
Asma Rahman <asma88rk@gmail.com>
2022-11-23 22:51:51 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const data1 = require("./Routes/Bollywood")
+
+app.use('/home',data1)
+app.listen(9000,()=>{
+    console.log("server is runningn");
+})
